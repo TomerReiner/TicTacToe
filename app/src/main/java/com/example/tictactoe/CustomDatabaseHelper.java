e6a3b545d188108e02fcb6d3df90c89d06f74846
Tomer Reiner <tomer3gaming2004@gmail.com>
2020-12-30 14:34:45 +0200

    Beta version of Tic Tac Toe game

---
+++ app/src/main/java/com/example/tictactoe/CustomDatabaseHelper.java
@@ -0,0 +1,33 @@
+package com.example.tictactoe;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class CustomDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int VERSION = 1;
+    public static final String DATABASE_NAME = "Users.db";
+
+    public CustomDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public CustomDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
